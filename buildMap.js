39e7739f7a847fcae412ba7562cacdeb54ba07f1
Cheng-Yuan-Lai <a186235@g,ail.com>
2023-12-12 16:33:05 +0800

    fix: esbuild error

--- buildMap.js
+++ buildMap.js
@@ -1,22 +1,32 @@
-import fs from 'fs/promises';
+import { readdir } from 'fs/promises';
+import { resolve, sep } from 'path';
 import esbuild from 'esbuild';
 
-const getDependencies = async () => {
-  const projectPackage = await fs.readFile('./package.json', 'utf8');
-  const packageJson = JSON.parse(projectPackage);
+const listNodeModules = async (dir = 'node_modules') => {
+  let results = [];
 
-  return Object.keys(packageJson.dependencies);
+  const files = await readdir(dir, { withFileTypes: true });
+
+  for (const file of files) {
+    if (file.isDirectory()) {
+      const res = resolve(dir, file.name);
+      results.push(res.split(sep).slice(-2).join('/'));
+      results = results.concat(await listNodeModules(res));
+    }
+  }
+  return results;
 };
 
-const shouldMarkAsExternal = async packageName => {
-  return packageName.includes('aws');
+const shouldMarkAsExternal = packageName => {
+  const externalKeywords = ['@aws-sdk', '@mapbox'];
+  return externalKeywords.some(keyword => packageName.includes(keyword));
+  // return packageName.includes('aws');
 };
 
 const buildMap = async (path, output) => {
   try {
-    const allDependencies = await getDependencies();
+    const allDependencies = await listNodeModules();
     const externalDependencies = allDependencies.filter(shouldMarkAsExternal);
-
     await esbuild.build({
       entryPoints: [path],
       bundle: true,
@@ -24,6 +34,7 @@ const buildMap = async (path, output) => {
       format: 'esm',
       platform: 'node',
       external: externalDependencies,
+      loader: { '.html': 'text' },
       outfile: output,
     });
   } catch (err) {
